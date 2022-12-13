67838f4afe4cd06a8c50dfbc1ba3568cea5821e5
Oren <oren.b.anderson@gmail.com>
2022-12-13 18:02:36 -0500

    Modified the radio code to be modular, still need to make it threadable, next update coming later

--- main.py
+++ main.py
@@ -2,17 +2,27 @@ from Practice import bno055_control
 import unit_test
 import dif_drive
 from Practice import servo_control
+from HardwareInterface import remote_nrf24
 
 
-def start_up():
-    unit_test.test_io()
+def startup_robot():
+    # unit_test.test_io()
     # led_control.led_launch()
     # servo_control.servo_drive_test()
     # bno055_control.bno055_setup()
-    differential_drive = dif_drive.DifferentialDrive(.105)
-    differential_drive.turn_to_angle(8)
+    # differential_drive = dif_drive.DifferentialDrive(.105)
+    # differential_drive.turn_to_angle(8)
+    initialize_radio()
 
+
+def exit_robot():
+    remote_nrf24.exit_radio()
 
 
 if __name__ == '__main__':
-    start_up()
+    try:
+        startup_robot()
+    except KeyboardInterrupt:
+        print(" Keyboard Interrupt detected. Powering down radio.")
+        exit_robot()
+
