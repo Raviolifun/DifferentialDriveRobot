fa6595a42b7ba0a8d26c8ddcdba6adc60a310092
Oren <oren.anderson@ufl.edu>
2022-12-13 15:51:35 -1000

    Added all the changes needed to test it on Frank... We will see now...

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 #import unit_test
 #import dif_drive
 #from Practice import servo_control
+from adafruit_servokit import ServoKit
 from HardwareInterface import remote_nrf24
 from threading import Thread
 import threading
 import time
+import math
 
 
 # Run these to start the robot, then call robot_loop() to continue
@@ -25,20 +27,54 @@ def exit_robot():
 
 
 # Make sure to NOT run this twice, currently there is no protection
+def control_mixing(joy_y, joy_x, input_norm, output_norm, threshold):
+    magnitude = joy_y/input_norm
+    direction = joy_x/input_norm
+
+    if abs(magnitude) * input_norm < threshold:
+        magnitude = 0
+
+    left = output_norm * magnitude * (1 + direction - direction * math.copysign(1, direction))
+    right = output_norm * magnitude * (1 - direction - direction * math.copysign(1, direction))
+
+    return left, right
+
+
 class RobotLoop(Thread):
-    def __init__(self):
+    def __init__(self, input_radio_thread):
         Thread.__init__(self)
+
+        # Add the radio as an object to pull data from
+        self.command_input = input_radio_thread
+
+        # I dont like putting this here, got to change this later
+        # TODO
+        self.kit = ServoKit(channels=16)
+
+        # Thread Overhead
         self.shutdown_flag = threading.Event()
         self.start()
         
     def run(self):
         while not self.shutdown_flag.is_set():
             self.robot_loop()
-    
+            time.sleep(25)
+
     # Main robot loop
     def robot_loop(self):
-        print("heya")
-        time.sleep(1)
+        address = self.command_input.self.address
+
+        if not address:
+            joy_y = self.command_input.self.joy_y
+            joy_x = self.command_input.self.joy_x
+            buttons = self.command_input.self.buttons
+
+            [left, right] = control_mixing(joy_y, joy_x, 2048, 1, 50)
+            self.kit.continuous_servo[0].throttle = left
+            self.kit.continuous_servo[1].throttle = right
+        else:
+            self.kit.continuous_servo[0].throttle = 0
+            self.kit.continuous_servo[1].throttle = 0
 
 
 # Start the robot from here
@@ -47,7 +83,7 @@ if __name__ == '__main__':
     startup_robot()
     # Start Robot stuff!
     radio_thread = remote_nrf24.RadioLoop()
-    robot_thread = RobotLoop()
+    robot_thread = RobotLoop(radio_thread)
     # Wait until something possibly errors out
 
     # If the user presses space, stop the device
