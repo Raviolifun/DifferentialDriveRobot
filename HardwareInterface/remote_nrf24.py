73a9aee0d66fb3c83cd0d717dc7b4b330e0f4d92
Oren <oren.anderson@ufl.edu>
2022-12-13 15:30:20 -1000

    Got threading working, now going to make the robot loop!

--- HardwareInterface/remote_nrf24.py
+++ HardwareInterface/remote_nrf24.py
@@ -8,110 +8,121 @@ import time
 import struct
 import threading
 from RF24 import RF24, RF24_PA_LOW
-
-########### USER CONFIGURATION ###########
-# See https://github.com/TMRh20/RF24/blob/master/pyRF24/readme.md
-# Radio CE Pin, CSN Pin, SPI Speed
-# CE Pin uses GPIO number with BCM and SPIDEV drivers, other platforms use
-# their own pin numbering
-# CS Pin addresses the SPI bus number at /dev/spidev<a>.<b>
-# ie: RF24 radio(<ce_pin>, <a>*10+<b>); spidev1.0 is 10, spidev1.1 is 11 etc..
-
-# Generic:
-radio = RF24(6, 0)
-
-# Variable to store values in
-payload = [None]*4
+from threading import Thread
 
 
-# def master():
-#     """Transmits an incrementing float every second"""
-#     radio.stopListening()  # put radio in TX mode
-#     failures = 0
-#     while failures < 6:
-#         # use struct.pack() to packet your data into the payload
-#         # "<f" means a single little endian (4 byte) float value.
-#         buffer = struct.pack("<f", payload[0])
-#         start_timer = time.monotonic_ns()  # start timer
-#         result = radio.write(buffer)
-#         end_timer = time.monotonic_ns()  # end timer
-#         if not result:
-#             print("Transmission failed or timed out")
-#             failures += 1
-#         else:
-#             print(
-#                 "Transmission successful! Time to Transmit:",
-#                 f"{(end_timer - start_timer) / 1000} us. Sent: {payload[0]}",
-#             )
-#             payload[0] += 0.01
-#         time.sleep(1)
-#     print(failures, "failures detected. Leaving TX role.")
+# Make sure to NOT run this twice, currently there is no protection
+class RadioLoop(Thread):
+    def __init__(self):
+        Thread.__init__(self)
 
+        # Initialize the radio
+        ########### USER CONFIGURATION ###########
+        # See https://github.com/TMRh20/RF24/blob/master/pyRF24/readme.md
+        # Radio CE Pin, CSN Pin, SPI Speed
+        # CE Pin uses GPIO number with BCM and SPIDEV drivers, other platforms use
+        # their own pin numbering
+        # CS Pin addresses the SPI bus number at /dev/spidev<a>.<b>
+        # ie: RF24 radio(<ce_pin>, <a>*10+<b>); spidev1.0 is 10, spidev1.1 is 11 etc..
 
-def slave(timeout=6):
-    """Listen for any payloads and print the transaction
+        # Generic:
+        self.radio = RF24(6, 0)
 
-    :param int timeout: The number of seconds to wait (with no transmission)
-        until exiting function.
-    """
-    radio.startListening()  # put radio in RX mode
+        # Variables to store values in
+        self.address = 0
+        self.joy_y = 0
+        self.joy_x = 0
+        self.buttons = 0
 
-    start_timer = time.monotonic()
-    while (time.monotonic() - start_timer) < timeout:
-        has_payload, pipe_number = radio.available_pipe()
-        if has_payload:
-            # fetch 1 payload from RX FIFO
-            buffer = radio.read(radio.payloadSize)
-            # use struct.unpack() to convert the buffer into usable data
-            # expecting a little endian float, thus the format string "<f"
-            # buffer[:4] truncates padded 0s in case payloadSize was not set
-            payload[0] = struct.unpack(">H", buffer[0:2])[0]
-            payload[1] = struct.unpack(">h", buffer[2:4])[0]
-            payload[2] = struct.unpack(">h", buffer[4:6])[0]
-            payload[3] = struct.unpack(">H", buffer[6:8])[0]
-            # print details about the received packet
-            print(
-                f"Received {radio.payloadSize} bytes",
-                f"on pipe {pipe_number}: {payload[0]}, {payload[1]}, {payload[2]}, {payload[3]}",
-            )
-            start_timer = time.monotonic()  # reset the timeout timer
+        # Call Initialization function
+        self.initialize_radio()
 
-    print("Nothing received in", timeout, "seconds. Leaving RX role")
-    # recommended behavior is to keep in TX mode while idle
-    radio.stopListening()  # put the radio in TX mode
+        # Handle Thread Stuff
+        self.shutdown_flag = threading.Event()
+        self.start()
 
+    def run(self):
+        self.slave(timeout=6)
 
-def exit_radio():
-    radio.powerDown()
+    # def master():
+    #     """Transmits an incrementing float every second"""
+    #     radio.stopListening()  # put radio in TX mode
+    #     failures = 0
+    #     while failures < 6:
+    #         # use struct.pack() to packet your data into the payload
+    #         # "<f" means a single little endian (4 byte) float value.
+    #         buffer = struct.pack("<f", payload[0])
+    #         start_timer = time.monotonic_ns()  # start timer
+    #         result = radio.write(buffer)
+    #         end_timer = time.monotonic_ns()  # end timer
+    #         if not result:
+    #             print("Transmission failed or timed out")
+    #             failures += 1
+    #         else:
+    #             print(
+    #                 "Transmission successful! Time to Transmit:",
+    #                 f"{(end_timer - start_timer) / 1000} us. Sent: {payload[0]}",
+    #             )
+    #             payload[0] += 0.01
+    #         time.sleep(1)
+    #     print(failures, "failures detected. Leaving TX role.")
 
+    def slave(self, timeout=6):
+        self.radio.startListening()  # put radio in RX mode
 
-def initialize_radio():
-    # Initialize the nRF24L01 on the spi bus
-    if not radio.begin():
-        raise RuntimeError("radio hardware is not responding")
+        start_timer = time.monotonic()
+        while (time.monotonic() - start_timer) < timeout and not self.shutdown_flag.is_set():
+            has_payload, pipe_number = self.radio.available_pipe()
+            if has_payload:
+                # fetch 1 payload from RX FIFO
+                buffer = self.radio.read(self.radio.payloadSize)
+                # use struct.unpack() to convert the buffer into usable data
+                # expecting a little endian float, thus the format string "<f"
+                # buffer[:4] truncates padded 0s in case payloadSize was not set
+                self.address = struct.unpack(">H", buffer[0:2])[0]
+                self.joy_y = struct.unpack(">h", buffer[2:4])[0]
+                self.joy_x = struct.unpack(">h", buffer[4:6])[0]
+                self.buttons = struct.unpack(">H", buffer[6:8])[0]
+                # print details about the received packet
+                print(
+                    f"Received {self.radio.payloadSize} bytes",
+                    f"on pipe {pipe_number}: {self.address}, {self.joy_y}, {self.joy_x}, {self.buttons}",
+                )
+                start_timer = time.monotonic()  # reset the timeout timer
 
-    # Set Address
-    address = [b"1Node", b"2Node"]
+        print("Nothing received in", timeout, "seconds or thread forcibly quit. Leaving RX role and turning radio off")
+        # recommended behavior is to keep in TX mode while idle
+        self.radio.stopListening()  # put the radio in TX mode
+        # Kill radio if it has been unresponsive too long
+        self.exit_radio()
 
-    # set the Power Amplifier level to -12 dBm since this test example is
-    # usually run with nRF24L01 transceivers in proximity of each other
-    radio.setPALevel(RF24_PA_LOW)  # RF24_PA_MAX is default
+    def exit_radio(self):
+        self.radio.powerDown()
 
-    # set the TX address of the RX node into the TX pipe
-    radio.openWritingPipe(address[1])  # always uses pipe 0
+    def initialize_radio(self):
+        # Initialize the nRF24L01 on the spi bus
+        if not self.radio.begin():
+            raise RuntimeError("radio hardware is not responding")
 
-    # set the RX address of the TX node into a RX pipe
-    radio.openReadingPipe(1, address[0])  # using pipe 1
+        # Set Address
+        address = [b"1Node", b"2Node"]
 
-    # Set payload to max size (transmission time is inconsequential)
-    radio.payloadSize = 32
+        # set the Power Amplifier level to -12 dBm since this test example is
+        # usually run with nRF24L01 transceivers in proximity of each other
+        self.radio.setPALevel(RF24_PA_LOW)  # RF24_PA_MAX is default
 
-    # for debugging, we have 2 options that print a large block of details
-    # (smaller) function that prints raw register values
-    # radio.printDetails()
-    # (larger) function that prints human-readable data
-    # radio.printPrettyDetails()
+        # set the TX address of the RX node into the TX pipe
+        self.radio.openWritingPipe(address[0])  # always uses pipe 0
 
-    slave(timeout=1)
+        # set the RX address of the TX node into a RX pipe
+        self.radio.openReadingPipe(1, address[1])  # using pipe 1
 
+        # Set payload to max size (transmission time is inconsequential)
+        self.radio.payloadSize = 32
+
+        # for debugging, we have 2 options that print a large block of details
+        # (smaller) function that prints raw register values
+        # radio.printDetails()
+        # (larger) function that prints human-readable data
+        # radio.printPrettyDetails()
 
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from Practice import bno055_control
-import unit_test
-import dif_drive
-from Practice import servo_control
+#from Practice import bno055_control
+#import unit_test
+#import dif_drive
+#from Practice import servo_control
 from HardwareInterface import remote_nrf24
+from threading import Thread
+import threading
+import time
 
 
+# Run these to start the robot, then call robot_loop() to continue
 def startup_robot():
     # unit_test.test_io()
     # led_control.led_launch()
@@ -12,17 +16,55 @@ def startup_robot():
     # bno055_control.bno055_setup()
     # differential_drive = dif_drive.DifferentialDrive(.105)
     # differential_drive.turn_to_angle(8)
-    initialize_radio()
+    pass
 
 
+# Make sure to safely exit and call these before the robot powers down
 def exit_robot():
-    remote_nrf24.exit_radio()
+    pass
 
 
+# Make sure to NOT run this twice, currently there is no protection
+class RobotLoop(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.shutdown_flag = threading.Event()
+        self.start()
+        
+    def run(self):
+        while not self.shutdown_flag.is_set():
+            self.robot_loop()
+    
+    # Main robot loop
+    def robot_loop(self):
+        print("heya")
+        time.sleep(1)
+
+
+# Start the robot from here
 if __name__ == '__main__':
+    # Startup robot peripherals
+    startup_robot()
+    # Start Robot stuff!
+    radio_thread = remote_nrf24.RadioLoop()
+    robot_thread = RobotLoop()
+    # Wait until something possibly errors out
+
+    # If the user presses space, stop the device
     try:
-        startup_robot()
+        while True:
+            pass
     except KeyboardInterrupt:
-        print(" Keyboard Interrupt detected. Powering down radio.")
-        exit_robot()
+        pass
+
+    print("Exiting Robot Program")
+
+    # Shutdown Robot Threads
+    robot_thread.shutdown_flag.set()
+    robot_thread.join()
+    radio_thread.shutdown_flag.set()
+    radio_thread.join()
+
+    # Call any required exit functions
+    exit_robot()
 
